430e340bdaf09918647eb189e66db2c8ec57fad0
miles <milesconlu13@gmail.com>
2024-11-28 12:21:40 -0500

    1.1

--- nestedDictionary.py
+++ nestedDictionary.py
@@ -1,17 +1,20 @@
-allguestItems = {'Fruits': {'Bananas':'5'}, 'Appliances': {}, 'Mobile Phones': {}}
+allguestItems = {'Fruits': {'Bananas':'5','Warlord':'55'}, 'Appliances': {}, 'Mobile Phones': {}}
 
 print ('Here are the available category you can choose from, choose one \n'
        'quit to exit.')
 for k in allguestItems.keys():
     print(k)
 
-def itemValues(itsValues):
-    valuez = ', '.join(itsValues.values())
-    return valuez if valuez else 'none'
+def itemValues(itsValues, valuez):
+
+        return valuez if valuez else 'none'
+
 
 def itemKeys(itsKeys):
-    keys = ','.join(itsKeys.keys())
-    return keys if keys else 'none'
+    item_list = ''
+    for item, quantity in itsKeys.items():
+        item_list += f"{item} {quantity}\n"
+    return item_list
 
 while True:
     category = input("\nEnter a category: ").strip()
@@ -20,7 +23,12 @@ while True:
         break
 
     if category in allguestItems:
-        print(f"You selected {category}")
-        keys = itemKeys(allguestItems[category])
-        valuez = itemValues(allguestItems[category])
-        print(f"{keys},{valuez}")
+        print(f"You selected {category} with items \n" + str(itemKeys(allguestItems[category]) ))
+
+
+    else:
+        allguestItems[category] = {}
+        itemVal = input('Enter a nested key and its value: '.strip())
+        itemKey = input()
+        allguestItems[category][itemVal] = itemKey
+        print(f"Added! {category}, {itemVal}, {itemKey}")
